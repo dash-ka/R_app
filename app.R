7655e3aef2ef8c12f729f7cb3c43489d409bedfc
Dash.ka <76863495+dash-ka@users.noreply.github.com>
2021-04-08 19:12:01 +0200

    Update app.R

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(visNetwork)
 library(dplyr)
 library(tidyr)
 
-data<- read.csv("www/train.csv")
+data<- read.csv("train.csv")
 titanic <- tibble(data) %>%                
   select(Survived, Pclass, Sex, Age, SibSp, Parch, Fare, Embarked) %>% 
   filter(Embarked!= "" ) %>% 
